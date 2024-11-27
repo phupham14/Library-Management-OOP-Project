f4a2d0c3f0d755be83e7e4c0336fe3d5bf4fb764
Phú Phạm <phamnguyenphu14@gmail.com>
2024-11-27 23:02:33 +0700

    Connect postgres db

---
+++ hust_lib/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args){
+        ConnectionUtil db = new ConnectionUtil();
+        db.connect_to_db("hust_lib", "hustlib_admin", "hustlib_admin");
+    }
+}
